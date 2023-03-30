6b07485ff672fda03514bd538a53f31d8476bb2d
Greattie <gamubode@gmail.com>
2023-03-30 13:18:54 +0100

    debugging

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
-const btn = document.querySelectorAll('.btn')
-const ans= document.querySelectorAll('.ans')
+const btn = document.querySelectorAll(".btn");
+const ans = document.querySelectorAll(".ans");
 
-btn.forEach(function(item){
-    item.addEventListener('click', function(){
-        ans.forEach(function(answer){
-            if(answer.classList.contains('show')){
-            answer.classList.remove('show')
-            }
-            else{
-                answer.classList.add('show')
-            }
-        })
-    })
-})
-// btn.addEventListener('click', function(){
-//     if(ans.classList.contains('show')){
-//         ans.classList.remove('show')
-//     }
-//     else{
-//         ans.classList.add('show')
-//     }
+// btn.forEach(function(item){
+//     item.addEventListener('click', function(){
+//         ans.forEach(function(answer){
+//             if(answer.classList.contains('show')){
+//             answer.classList.remove('show')
+//             }
+//             else{
+//                 answer.classList.add('show')
+//             }
+//         })
+//     })
 // })
+
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", function () {
+    console.log(btn[i]);
+    for (let j = 0; j < ans.length; j++) {
+      console.log(ans[j]);
+      if (ans.classList.contains("show")) {
+        ans.classList.remove("show");
+      } else {
+        ans.classList.add("show");
+      }
+    }
+  });
+
+  // buttons.addEventListener('click', function(){
+  //     for(let j =0; j<ans.length; j++){
+  //         console.log(ans[j])
+  //     }
+  // })
+}
