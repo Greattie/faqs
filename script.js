8fdae110bd7e8ba74fc5426c022fc18fcc455c55
Greattie <gamubode@gmail.com>
2023-04-12 13:05:11 +0100

    js debugging

--- script.js
+++ script.js
@@ -1,35 +1,9 @@
 const btn = document.querySelectorAll(".btn");
-const ans = document.querySelectorAll(".ans");
 
-// btn.forEach(function(item){
-//     item.addEventListener('click', function(){
-//         ans.forEach(function(answer){
-//             if(answer.classList.contains('show')){
-//             answer.classList.remove('show')
-//             }
-//             else{
-//                 answer.classList.add('show')
-//             }
-//         })
-//     })
-// })
-
-for (let i = 0; i < btn.length; i++) {
-  btn[i].addEventListener("click", function () {
-    console.log(btn[i]);
-    for (let j = 0; j < ans.length; j++) {
-      console.log(ans[j]);
-      if (ans.classList.contains("show")) {
-        ans.classList.remove("show");
-      } else {
-        ans.classList.add("show");
-      }
-    }
+btn.forEach(function (button) {
+  console.log(button);
+  button.addEventListener("click", function (e) {
+    const showText = e.currentTarget.parentElement.nextElementSibling;
+    showText.classList.toggle("ans");
   });
-
-  // buttons.addEventListener('click', function(){
-  //     for(let j =0; j<ans.length; j++){
-  //         console.log(ans[j])
-  //     }
-  // })
-}
+});
